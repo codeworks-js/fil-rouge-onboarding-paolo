b6f0f555ea4347655358fe1e0200a58bfc640b53
Paolo Manaois <paolo.manaois@codeworks.fr>
2023-10-06 10:00:25 +0200

    refactor: switched to Route components

--- src/router/AppRouter.tsx
+++ src/router/AppRouter.tsx
@@ -1,34 +1,20 @@
-import { RouterProvider, createBrowserRouter } from "react-router-dom";
+import { Route, RouterProvider, createBrowserRouter, createRoutesFromElements } from "react-router-dom";
 import Dashboard from "../components/dashboard/Dashboard";
 import HeroDetail from "../components/heroes/HeroDetail";
 import Heroes from "../components/heroes/Heroes";
 import Root from "../components/root/Root";
 import { ENDPOINTS } from "./endpoints";
 
-const router = createBrowserRouter([
-    {
-        path: ENDPOINTS.INDEX,
-        element: <Root/>,
-        children: [
-            {
-                path: ENDPOINTS.DASHBOARD,
-                element: <Dashboard/>
-            },
-            {
-                path: ENDPOINTS.HERO_DETAILS,
-                element: <HeroDetail/>
-            },
-            {
-                path: ENDPOINTS.HEROES,
-                element: <Heroes/>
-            },
-            {
-                index: true,
-                element: <Dashboard/>
-            }
-        ]
-    }
-])
+const router = createBrowserRouter(
+    createRoutesFromElements(
+        <Route path="/" element={<Root/>}>
+            <Route index element={<Dashboard/>}/>
+            <Route path={ENDPOINTS.DASHBOARD} element={<Dashboard/>} />
+            <Route path={ENDPOINTS.HEROES} element={<Heroes/>} />
+            <Route path={ENDPOINTS.HERO_DETAILS} element={<HeroDetail/>} />
+        </Route>
+    )
+)
 
 function AppRouter() {
     return <RouterProvider router={router}/>
